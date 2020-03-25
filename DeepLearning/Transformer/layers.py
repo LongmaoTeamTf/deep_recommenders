b0e0f794f537ca142e095e6201755ec7659c9088
王垚 <wangyao.sr@gmail.com>
2020-03-25 17:36:26 +0800

    add tips

--- DeepLearning/Transformer/layers.py
+++ DeepLearning/Transformer/layers.py
@@ -5,7 +5,7 @@
 @Author: Wang Yao
 @Date: 2020-03-22 17:48:05
 @LastEditors: Wang Yao
-@LastEditTime: 2020-03-25 17:22:39
+@LastEditTime: 2020-03-25 17:35:58
 '''
 from __future__ import print_function
 
@@ -278,6 +278,7 @@ if __name__ == "__main__":
     batch_size = 256
     epochs = 20
 
+    print('Model building ... ')
     inputs = Input(shape=(max_len,), name="inputs")
     masks = Input(shape=(max_len,), name='masks')
     embeddings = Embedding(vocab_size, model_dim)(inputs)
@@ -291,7 +292,8 @@ if __name__ == "__main__":
     model = Model(inputs=[inputs, masks], outputs=outputs)
     model.compile(optimizer=Adam(beta_1=0.9, beta_2=0.98, epsilon=1e-9), 
         loss='categorical_crossentropy', metrics=['accuracy'])
-
+    
+    print("Data downloading and processing ... ")
     (x_train, y_train), (x_test, y_test) = imdb.load_data(maxlen=max_len, num_words=vocab_size)
     x_train = sequence.pad_sequences(x_train, maxlen=max_len)
     x_test = sequence.pad_sequences(x_test, maxlen=max_len)
@@ -299,4 +301,6 @@ if __name__ == "__main__":
     x_test_masks = np.array([tf.equal(x, 0) for x in x_test])
     y_train = to_categorical(y_train)
     y_test = to_categorical(y_test)
+    
+    print("Model Training ... ")
     model.fit([x_train, x_train_masks], y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2)
