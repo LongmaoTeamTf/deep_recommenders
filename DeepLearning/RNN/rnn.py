aa8752beacb4bcf2a101db3dce1509dae41f46d0
王垚 <wangyao.sr@gmail.com>
2020-03-31 16:05:26 +0800

    Add Birnn

--- DeepLearning/RNN/rnn.py
+++ DeepLearning/RNN/rnn.py
@@ -5,7 +5,7 @@
 @Author: Wang Yao
 @Date: 2020-03-30 15:47:00
 @LastEditors: Wang Yao
-@LastEditTime: 2020-03-31 14:19:35
+@LastEditTime: 2020-03-31 16:04:42
 '''
 import os
 import numpy as np
@@ -48,15 +48,15 @@ class RNN(Layer):
     def __init__(self, 
             kernel_dim,
             activation='tanh',
-            return_ots=False, 
-            return_state=False,
+            return_outputs=False, 
+            return_states=False,
             use_bias=True,
             **kwargs):
         super(RNN, self).__init__(**kwargs)
         self._kernel_dim = kernel_dim
         self._activation = activations.get(activation)
-        self._return_ots = return_ots
-        self._return_state = return_state
+        self._return_outputs = return_outputs
+        self._return_states = return_states
         self._use_bias = use_bias
         
     def build(self, input_shape):
@@ -71,27 +71,30 @@ class RNN(Layer):
             initializer='glorot_uniform',
             trainable=True,
             name='weights_hidden')
-        # self.V = self.add_weight(
-        #     shape=(self._kernel_dim, self._kernel_dim),
-        #     initializer='glorot_uniform',
-        #     trainable=True,
-        #     name='weights_output')
         if self._use_bias:
             self.b = self.add_weight(
                 shape=(self._kernel_dim),
                 initializer='zeros',
                 trainable=True,
                 name='bias_b')
-            # self.c = self.add_weight(
-            #     shape=(self._kernel_dim),
-            #     initializer='zeros',
-            #     trainable=True,
-            #     name='bias_c')
+        if self._return_outputs:
+            self.V = self.add_weight(
+                shape=(self._kernel_dim, self._kernel_dim),
+                initializer='glorot_uniform',
+                trainable=True,
+                name='weights_output')
+            if self._use_bias:
+                self.c = self.add_weight(
+                shape=(self._kernel_dim),
+                initializer='zeros',
+                trainable=True,
+                name='bias_c')
+        
         super(RNN, self).build(input_shape)
 
     def call(self, inputs):
         h_t = K.zeros((1, self._kernel_dim))
-        ots = []
+        ots, hts = []
         for t in range(inputs.shape[1]):
             x_t = K.expand_dims(inputs[:, t, :], 1)
             a_t = K.dot(x_t, self.U) + K.dot(h_t, self.W)
@@ -99,19 +102,21 @@ class RNN(Layer):
 
             if self._activation is not None:
                 h_t = self._activation(a_t)
+                hts.append(h_t)
 
-            # o_t = K.dot(h_t, self.V)
-            # if self._use_bias: o_t += self.c
-            # y_t = K.softmax(o_t)
-            # ots.append(y_t)
-        # outputs = ots[-1]
-        # if self._return_ots:
-        #     outputs = ots
-        # if self._return_state:
-        #     outputs = [ots[-1], h_t]
-        # if self._return_ots and self._return_state:
-        #     outputs = [ots, h_t]
-        return h_t
+            if self._return_outputs:
+                o_t = K.dot(h_t, self.V)
+                if self._use_bias: o_t += self.c
+                o_t = K.softmax(o_t)
+                ots.append(o_t)
+        outputs = h_t
+        if self._return_outputs:
+            outputs = ots
+        if self._return_states:
+            outputs = hts
+        if self._return_outputs and self._return_states:
+            outputs = ots, hts 
+        return outputs
 
     def compute_output_shape(self, input_shape):
         return [input_shape[:-1] + (self._kernel_dim,), 
@@ -120,16 +125,60 @@ class RNN(Layer):
 
 class BiDirectional(Layer):
     
-    def __init__(self, rnn_cell,  **kwargs):
+    def __init__(self, rnn_cell, merge_mode='concat', **kwargs):
         super(BiDirectional, self).__init__(**kwargs)
+        if merge_mode not in ['sum', 'mul', 'ave', 'concat', None]:
+            raise ValueError('Invalid merge mode. '
+                             'Merge mode should be one of '
+                             '{"sum", "mul", "ave", "concat", None}')
         self._rnn_cell = rnn_cell
+        self._merge_mode = merge_mode
+        self._return_outputs = rnn_cell._return_outputs
+        self._return_states = rnn_cell._return_states
 
     def call(self, inputs):
-        return 
+        reverse_inputs = K.reverse(inputs, 1)
+        rnn_fw_outputs = self._rnn_cell(inputs)
+        rnn_bw_outputs = self._rnn_cell(reverse_inputs)
+        
+        if self._return_states:
+            if self._return_outputs:
+                _, fw_states = rnn_fw_outputs
+                _, bw_states = rnn_bw_outputs
+            else:
+                fw_states = rnn_fw_outputs
+                bw_states = rnn_bw_outputs
+        else:
+            raise ValueError("BiDirectional rnn cell must set `_return_states` to True.")
+        
+        fw_states, bw_states = K.reverse(fw_states, 1), K.reverse(bw_states, 1)
+
+        if self._merge_mode == 'concat':
+            outputs = K.concatenate([fw_states, bw_states])
+        elif self._merge_mode == 'sum':
+            outputs = fw_states + bw_states
+        elif self._merge_mode == 'ave':
+            outputs = (fw_states + bw_states) / 2
+        elif self._merge_mode == 'mul':
+            outputs = fw_states * bw_states
+        elif self._merge_mode is None:
+            outputs = [fw_states, bw_states]
+        else:
+            raise ValueError('Unrecognized value for argument '
+                             'merge_mode: %s' % (self._merge_mode))
+        return outputs
 
 
     def compute_output_shape(self, input_shape):
-        return input_shape
+        output_shape = self._rnn_cell.compute_output_shape(input_shape)
+        if self._return_states:
+            if self._return_outputs:
+                _, output_shape = output_shape
+        if self._merge_mode == 'concat':
+            output_shape = output_shape[:-1] + (output_shape[-1]*2)
+        elif self._merge_mode is None:
+            output_shape = [output_shape, output_shape]
+        return output_shape
 
     
 
@@ -141,6 +190,7 @@ if __name__ == "__main__":
     from tensorflow.keras.datasets import imdb
     from tensorflow.keras.preprocessing import sequence
     from tensorflow.keras.utils import to_categorical
+    
 
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -160,8 +210,8 @@ if __name__ == "__main__":
     print('Model building ... ')
     inputs = Input(shape=(max_len,), name="inputs")
     embeddings = Embedding(vocab_size, model_dim)(inputs)
-    ots = RNN(model_dim)(embeddings)
-    x = GlobalAveragePooling1D()(ots)
+    outputs = BiDirectional(RNN(model_dim))(embeddings)
+    x = GlobalAveragePooling1D()(outputs)
     x = Dropout(0.2)(x)
     x = Dense(10, activation='relu')(x)
     outputs = Dense(2, activation='softmax')(x)
