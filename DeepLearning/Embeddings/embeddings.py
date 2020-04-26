d6939ea13a349443e929e09237d0a9c17785bd48
王垚 <wangyao.sr@gmail.com>
2020-04-26 19:14:19 +0800

    Add trainable

--- DeepLearning/Embeddings/embeddings.py
+++ DeepLearning/Embeddings/embeddings.py
@@ -5,7 +5,7 @@
 @Author: Wang Yao
 @Date: 2020-03-31 17:55:21
 @LastEditors: Wang Yao
-@LastEditTime: 2020-04-02 23:25:13
+@LastEditTime: 2020-04-07 10:55:29
 '''
 import numpy as np
 import tensorflow as tf
@@ -16,16 +16,18 @@ from tensorflow.keras.layers import Layer
 
 class Embedding(Layer):
 
-    def __init__(self, vocab_size, model_dim, scale=True, **kwargs):
+    def __init__(self, vocab_size, model_dim, scale=True, trainable=True, **kwargs):
         self._vocab_size = vocab_size
         self._model_dim = model_dim
         self._scale = scale
+        self._trainable = trainable
         super(Embedding, self).__init__(**kwargs)
 
     def build(self, input_shape):
         self.embeddings = self.add_weight(
             shape=(self._vocab_size, self._model_dim),
             initializer='glorot_uniform',
+            trainable=self._trainable,
             name="embeddings")
         super(Embedding, self).build(input_shape)
 
