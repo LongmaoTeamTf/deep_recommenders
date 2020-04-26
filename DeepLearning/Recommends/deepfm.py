9d3c192b076fefbf837ed3418483abbcf9a8c1e6
王垚 <wangyao.sr@gmail.com>
2020-04-26 19:14:38 +0800

    Add DeepFM

---
+++ DeepLearning/Recommends/deepfm.py
@@ -0,0 +1,199 @@
+'''
+@Description: 
+@version: 
+@License: MIT
+@Author: Wang Yao
+@Date: 2020-04-07 10:51:06
+@LastEditors: Wang Yao
+@LastEditTime: 2020-04-07 11:11:36
+'''
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from tensorflow.keras.layers import Layer
+from tensorflow.keras import activations
+
+
+class OneOrder(Layer):
+
+    def __init__(self, sparse_values_size, **kwargs):
+        self._sparse_values_size = sparse_values_size
+        super(OneOrder, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.n_sparse = len(input_shape[0])
+        self.n_denses = len(input_shape[1])
+        self.sparse_weights = []
+        for i in range(self.n_sparse):
+            self.sparse_weights.append(self.add_weight(
+                shape=(self._sparse_values_size[i], 1),
+                initializer="glorot_uniform",
+                trainable=True,
+                name=f'sparse_weights_{i}'))
+        self.dense_weights = self.add_weight(
+            shape=(self.n_denses, 1),
+            initializer="glorot_uniform",
+            trainable=True,
+            name='dense_weights')
+        super(OneOrder, self).build(input_shape)
+
+    def call(self, inputs):
+        sparse_inputs, dense_inputs = inputs
+        outputs = 0
+        for i in range(self.n_sparse):
+            sparse_w = K.gather(self.sparse_weights[i], sparse_inputs[i])
+            outputs += sparse_w[:, 0, :]
+        dense_inputs = K.concatenate(dense_inputs)
+        outputs += K.dot(dense_inputs, self.dense_weights)
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0][0][0], 1)
+
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({
+            'sparse_values_size': self._sparse_values_size})
+        return config
+
+
+class EmbeddingLayer(Layer):
+
+    def __init__(self, sparse_values_size, embedding_dim, **kwargs):
+        self._sparse_values_size = sparse_values_size
+        self._embedding_dim = embedding_dim
+        super(EmbeddingLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.n_sparse = len(input_shape[0])
+        self.n_denses = len(input_shape[1])
+        self.sparse_weights = []
+        for i in range(self.n_sparse):
+            self.sparse_weights.append(self.add_weight(
+                shape=(self._sparse_values_size[i], self._embedding_dim),
+                initializer="glorot_uniform",
+                # regularizer=regularizers.l2(0.5),
+                trainable=True,
+                name=f'sparse_weights_{i}'))
+        self.dense_weights = []
+        for i in range(self.n_denses):
+            self.dense_weights.append(self.add_weight(
+                shape=(1, self._embedding_dim),
+                initializer="glorot_uniform",
+                # regularizer=regularizers.l2(0.5),
+                trainable=True,
+                name=f'dense_weights_{i}'))
+        super(EmbeddingLayer, self).build(input_shape)
+
+    def call(self, inputs):
+        sparse_inputs, dense_inputs = inputs
+        embeddings = []
+        for i in range(self.n_sparse):
+            sparse_w = K.gather(self.sparse_weights[i], sparse_inputs[i])
+            embeddings.append(sparse_w[:, 0, :])
+        for i in range(self.n_denses):
+            embeddings.append(K.dot(dense_inputs[i], self.dense_weights[i]))
+        return embeddings
+
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({
+            'sparse_values_size': self._sparse_values_size,
+            'embedding_dim': self._embedding_dim})
+        return config
+
+
+class TwoOrder(Layer):
+
+    def __init__(self, **kwargs):
+        super(TwoOrder, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.output_weight = self.add_weight(
+            shape=(input_shape[0][-1], 1),
+            initializer="glorot_uniform",
+            trainable=True,
+            name='output_weight')
+        super(TwoOrder, self).build(input_shape)
+
+    def call(self, inputs):
+        exp_inputs = [K.expand_dims(x, axis=1) for x in inputs]
+        cocat_inputs = K.concatenate(exp_inputs, axis=1)
+        square_inputs = K.square(K.sum(cocat_inputs, axis=1))        
+        sum_inputs = K.sum(K.square(cocat_inputs),axis=1)
+        outputs = K.dot(square_inputs - sum_inputs, self.output_weight)
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0][0], 1)
+
+
+class HighOrder(Layer):
+
+    def __init__(self, n_layers, dropout_rate=0.5, activation='relu', **kwargs):
+        self._n_layers = n_layers
+        self._dropout_rate = dropout_rate
+        self._activation = activations.get(activation)
+        super(HighOrder, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.n_fields = len(input_shape)
+        self.kernel_size = input_shape[0][-1] * self.n_fields
+        self.hidden_weights = []
+        for i in range(self._n_layers):
+            self.weights.append(self.add_weight(
+                shape=(self.kernel_size, self.kernel_size),
+                initializer="glorot_uniform",
+                trainable=True,
+                name=f'hidden_weight_{i}'))
+        self.output_weight = self.add_weight(
+            shape=(self.kernel_size, 1),
+            initializer="glorot_uniform",
+            trainable=True,
+            name='output_weight')
+        super(HighOrder, self).build(input_shape)
+
+    def call(self, inputs):
+        outputs = K.concatenate(inputs, axis=1)
+        for i in range(self._n_layers):
+            outputs = K.dot(outputs, self.weights[i])
+            outputs = self._activation(outputs)
+            outputs = K.dropout(outputs, self._dropout_rate)
+        outputs = K.dot(outputs, self.output_weight)
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0][0], 1)
+
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({
+            'n_layers': self._n_layers,
+            'dropout_rate': self._dropout_rate,
+            'activation': self._activation})
+        return config
+
+
+class LR(Layer):
+
+    def __init__(self, **kwargs):
+        super(LR, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.w = self.add_weight(
+            shape=(3, 1),
+            initializer="glorot_uniform",
+            trainable=True,
+            name='w')
+        super(LR, self).build(input_shape)
+
+    def call(self, inputs):
+        inputs = K.concatenate(inputs, axis=1)
+        outputs = K.dot(inputs, self.w)
+        outputs = K.sigmoid(outputs)
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0][0], 1)
---
+++ DeepLearning/Recommends/train.py
@@ -0,0 +1,92 @@
+"""
+@Description: 
+@version: 
+@License: MIT
+@Author: Wang Yao
+@Date: 2020-04-26 17:47:37
+@LastEditors: Wang Yao
+@LastEditTime: 2020-04-26 19:13:50
+"""
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.utils import plot_model
+from deepfm import EmbeddingLayer
+from deepfm import OneOrder, TwoOrder, HighOrder
+from deepfm import LR
+from tqdm import tqdm
+from sklearn.preprocessing import LabelEncoder
+    
+
+def data_preparing(filepath, n_samples=50000):
+    data = []
+    n = 1
+    progress = tqdm(total=n_samples)
+    with open(filepath, "r") as f:
+        for line in f:
+            if n > n_samples:
+                break
+            line = line.strip()
+            line_data = line.split("\t")
+            line_data = [None if x == "" else x for x in line_data ]
+            data.append(line_data)
+            progress.update(1)
+            n += 1
+    progress.close()
+    df = pd.DataFrame(data)
+    integer_cols = [f"I{i}" for i in range(13)]
+    categorical_cols = [f"C{i}" for i in range(26)]
+    df.columns = ["label"] + integer_cols + categorical_cols
+    return df, integer_cols, categorical_cols
+
+
+def data_process(df, integer_cols, categorical_cols):
+    for col in integer_cols:
+        df[col] = df[col].fillna(0.)
+        df[col] = df[col].astype(np.float32)
+        df[col] = df[col].apply(lambda x: np.log(x+1) if x > -1 else -1)
+    encoder = LabelEncoder()
+    sparse_values_size = []
+    for col in categorical_cols:
+        df[col] = df[col].fillna("-1")
+        df[col] = encoder.fit_transform(df[col])
+        sparse_values_size.append(df[col].nunique())
+    return df, sparse_values_size
+
+
+def build_model(integer_cols, categorical_cols, sparse_values_size, embedding_dim=10):
+    sparse_inputs = [Input(shape=(1,), dtype=tf.int32, name=col) for col in categorical_cols] 
+    dense_inputs = [Input(shape=(1,), dtype=tf.float32, name=col) for col in integer_cols]
+    one_order_outputs = OneOrder(sparse_values_size)([sparse_inputs, dense_inputs])
+    embeddings = EmbeddingLayer(sparse_values_size, embedding_dim)([sparse_inputs, dense_inputs])
+    two_order_outputs = TwoOrder()(embeddings)
+    high_order_outputs = HighOrder(2)(embeddings)
+    outputs = LR()([one_order_outputs, two_order_outputs, high_order_outputs])
+    model = Model(inputs=[sparse_inputs, dense_inputs], outputs=outputs)
+    return model
+
+
+if __name__ == "__main__":
+    filepath = "/home/xddz/code/eyepetizer_recommends/data/dac/train.txt"
+    # filepath = "/Users/wangyao/Desktop/eyepetizer/dac/train.txt"
+    # 数据准备
+    df, integer_cols, categorical_cols = data_preparing(filepath, n_samples=500000)
+    # 数据处理
+    df, sparse_values_size = data_process(df, integer_cols, categorical_cols)
+    # 生成训练数据
+    sparse_inputs = [df[col].values for col in categorical_cols]
+    dense_inputs = [df[col].values for col in integer_cols]
+    targets = df['label'].astype(np.float32).values
+    # 构建模型
+    model = build_model(integer_cols, categorical_cols, sparse_values_size)
+    plot_model(model, "deepfm.png")
+    # 训练模型
+    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
+    model.fit(
+        sparse_inputs + dense_inputs, 
+        targets, 
+        batch_size=256,
+        epochs=10, 
+        validation_split=0.2)
