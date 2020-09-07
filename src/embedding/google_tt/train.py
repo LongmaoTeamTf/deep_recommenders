d0d187b6612b5935d5b7c5e3a03477372d22b72b
王垚 <wangyao.sr@gmail.com>
2020-09-07 10:55:38 +0800

    Fix sampling p estimate

--- src/embedding/google_tt/train.py
+++ src/embedding/google_tt/train.py
@@ -5,11 +5,12 @@
 @Author: Wang Yao
 @Date: 2020-08-26 20:47:47
 @LastEditors: Wang Yao
-@LastEditTime: 2020-09-03 16:37:38
+@LastEditTime: 2020-09-07 10:43:36
 """
 import functools
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 
 def parse_csv_line(left_columns,
@@ -108,13 +109,13 @@ def get_dataset_from_csv_files(filenames,
     return dataset
 
 
-def sampling_p_estimation_single_hash(array_a, array_b, hash_indexs, global_step, alpha=0.001):
+def sampling_p_estimation_single_hash(array_a, array_b, hash_indexs, global_step, alpha=0.01):
     """单Hash函数采样概率估计"""
     array_b[hash_indexs] = (1 - alpha) * array_b[hash_indexs] + \
                                 alpha * (global_step - array_a[hash_indexs])
     array_a[hash_indexs] = global_step
     sampling_p = 1 / array_b[hash_indexs]
-    return sampling_p
+    return array_a, array_b, sampling_p
 
 
 def hash_simple(ids, hash_bucket_size):
@@ -142,15 +143,21 @@ def corrected_batch_softmax(x, y, sampling_p):
 @tf.function
 def reward_cross_entropy(reward, output):
     """Reward correction batch """
-    return -tf.reduce_sum(reward * tf.math.log(output))
+    return -tf.reduce_mean(reward * tf.math.log(output))
 
 
-def custom_train_model(left_model, right_model, train_dataset, train_steps, num_epochs):
+def custom_train_model(left_model, 
+                       right_model, 
+                       train_dataset, 
+                       train_steps,
+                       valid_dataset,
+                       valid_steps,
+                       num_epochs,
+                       ids_column,
+                       ids_hash_bucket_size,
+                       beta=100,
+                       lr=0.01):
     """自定义训练"""
-
-    _hash_bucket_size = 5
-    _array_b_init = 100
-    _lr = 0.01
 
     @tf.function
     def loss(left_x, right_x, sampling_p, reward):
@@ -167,32 +174,67 @@ def custom_train_model(left_model, right_model, train_dataset, train_steps, num_
         right_grads = tape.gradient(loss_value, right_model.trainable_variables)
         return loss_value, left_grads, right_grads
 
-    optimizer = tf.keras.optimizers.Adagrad(learning_rate=_lr)
+    optimizer = tf.keras.optimizers.Adadelta(learning_rate=lr)
     
-    array_a = np.zeros(shape=(_hash_bucket_size,), dtype=np.float32)
-    array_b = np.ones(shape=(_hash_bucket_size,), dtype=np.float32) * _array_b_init
+    array_a = np.zeros(shape=(ids_hash_bucket_size,), dtype=np.float32)
+    array_b = np.ones(shape=(ids_hash_bucket_size,), dtype=np.float32) * beta
     train_loss_results = []
+    valid_loss_results = []
+    
+    # summary_writer = tf.summary.create_file_writer('./logs')
 
     for epoch in range(num_epochs):
-        epoch_loss_avg = tf.keras.metrics.Mean()
-        step = 0
+        epoch_train_loss_avg = tf.keras.metrics.Mean()
+        train_step = 0
         for left_x, right_x, reward in train_dataset:
-            cand_ids = right_x.get('cand_id')
-            cand_hash_indexs = hash_simple(cand_ids, 5)
-            sampling_p = sampling_p_estimation_single_hash(array_a, array_b, cand_hash_indexs, step)
-
+            cand_ids = right_x.get(ids_column)
+            cand_hash_indexs = hash_simple(cand_ids, ids_hash_bucket_size)
+            array_a, array_b, sampling_p = sampling_p_estimation_single_hash(array_a, array_b, cand_hash_indexs, train_step)
+            
             loss_value, left_grads, right_grads = grad(left_x, right_x, sampling_p, reward)
             optimizer.apply_gradients(zip(left_grads, left_model.trainable_variables))
             optimizer.apply_gradients(zip(right_grads, right_model.trainable_variables))
 
-            epoch_loss_avg(loss_value)
-            step += 1
-            print("Epoch {:03d}/{:03d}: {}/{} train-correct-sfx: {:.3f}".format(
-                epoch+1, num_epochs, step, train_steps, loss_value), end='\r')
+            epoch_train_loss_avg(loss_value)
+            train_step += 1
+            progress = '='*int(train_step/train_steps*50)+'>'+' '*(50-int(train_step/train_steps*50))
+            print("\rEpoch {:03d}/{:03d}: Train - {}/{} [{}] correct-sfx: {:.3f}".format(
+                epoch+1, num_epochs, train_step, train_steps, progress, loss_value), end='', flush=True)
 
-        train_loss_results.append(epoch_loss_avg.result())
+        train_loss_results.append(epoch_train_loss_avg.result())
+    
+        print("\nEpoch {:03d}/{:03d}: train-epoch-avg-correct-sfx: {:.3f}".format(
+            epoch+1, num_epochs, epoch_train_loss_avg.result()))
+        
+        epoch_valid_loss_avg = tf.keras.metrics.Mean()
+        valid_step = 0
+        for left_x, right_x, reward in valid_dataset:
+            cand_ids = right_x.get(ids_column)
+            cand_hash_indexs = hash_simple(cand_ids, ids_hash_bucket_size)
+            sampling_p = 1 / array_b[cand_hash_indexs]
+            loss_value, left_grads, right_grads = grad(left_x, right_x, sampling_p, reward)
+            epoch_valid_loss_avg(loss_value)
+            valid_step += 1
+            progress = '='*int(valid_step/valid_steps*20)+'>'+' '*(20-int(valid_step/valid_steps*20))
+            print("\rEpoch {:03d}/{:03d}: Valid - {}/{} [{}] correct-sfx: {:.3f}".format(
+                epoch+1, num_epochs, valid_step, valid_steps, progress, loss_value), end='', flush=True)
 
-        print("\nEpoch {:03d}/{:03d}: train-correct-sfx: {:.3f}".format(
-            epoch+1, num_epochs, epoch_loss_avg.result()))
+        valid_loss_results.append(epoch_valid_loss_avg.result())
+    
+        print("\nEpoch {:03d}/{:03d}: valid-epoch-avg-correct-sfx: {:.3f}".format(
+            epoch+1, num_epochs, epoch_valid_loss_avg.result()))
 
+        # with summary_writer.as_default(): # pylint: disable=not-context-manager
+        #     tf.summary.scalar('train_loss', epoch_loss_avg.result(), step=epoch)
+        
     return left_model, right_model
+
+
+def plot_metric(metric_results, metric_name):
+    """metrics变化曲线"""
+    plt.xlabel('Epoch')
+    plt.ylabel(metric_name)
+    plt.plot(metric_results)
+    plt.show()
+
+    
