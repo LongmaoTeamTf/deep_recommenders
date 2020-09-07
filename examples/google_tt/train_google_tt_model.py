5c016c9bf02437e2c2c801c7131e48fe5cccfb5f
王垚 <wangyao.sr@gmail.com>
2020-09-07 15:22:08 +0800

    Debug

--- examples/google_tt/train_google_tt_model.py
+++ examples/google_tt/train_google_tt_model.py
@@ -5,14 +5,15 @@
 @Author: Wang Yao
 @Date: 2020-09-03 16:26:18
 @LastEditors: Wang Yao
-@LastEditTime: 2020-09-07 10:51:42
+@LastEditTime: 2020-09-07 15:21:31
 """
+import os
 import sys
 sys.path.append('../..')
 import math
 from src.embedding.google_tt.modeling import build_model
-from src.embedding.google_tt.train import custom_train_model
 from src.embedding.google_tt.train import get_dataset_from_csv_files
+from src.embedding.google_tt.train import train_model
 
 
 
@@ -28,7 +29,7 @@ if __name__ == "__main__":
         'seed_like_count',
         'seed_share_count',
         'seed_collect_count',
-        # 'seed_reply_count',   
+        'seed_reply_count'
     ]
     right_columns = [
         'cand_id',
@@ -40,7 +41,7 @@ if __name__ == "__main__":
         'cand_like_count',
         'cand_share_count',
         'cand_collect_count',
-        # 'cand_reply_count',
+        'cand_reply_count'
     ]
     csv_header = [
         'label',
@@ -55,7 +56,7 @@ if __name__ == "__main__":
         'seed_like_count',
         'seed_share_count',
         'seed_collect_count',
-        # 'seed_reply_count',
+        'seed_reply_count',
         'cand_id',
         'cand_category',
         'cand_tags',
@@ -65,48 +66,56 @@ if __name__ == "__main__":
         'cand_like_count',
         'cand_share_count',
         'cand_collect_count',
-        # 'cand_reply_count',
+        'cand_reply_count'
     ]
     
-    train_dataset = get_dataset_from_csv_files(
-        [
-            '/home/xddz/data/two_tower_data/2020-09-01.csv',
-            '/home/xddz/data/two_tower_data/2020-09-02.csv'
-        ], 
-        left_columns, 
-        right_columns, 
-        csv_header, 
-        batch_size=256
-    )
+    def _get_steps(fns, batch_size, skip_header=True):
+        _total_num = 0
+        for fn in fns:
+            cmd = "wc -l < {}".format(fn)
+            cmd_res = os.popen(cmd)
+            _num_lines = int(cmd_res.read().strip())
+            if skip_header is True:
+                _num_lines -= 1
+            _total_num += _num_lines
+        _steps = math.ceil(_total_num / batch_size)
+        return _steps
 
-    valid_dataset = get_dataset_from_csv_files(
-        [
-            '/home/xddz/data/two_tower_data/2020-09-03.csv'
-        ], 
+    filenames = [
+        '/home/xddz/data/two_tower_data/2020-09-01.csv',
+        '/home/xddz/data/two_tower_data/2020-09-02.csv',
+        '/home/xddz/data/two_tower_data/2020-09-03.csv'
+    ]
+    batch_size = 512
+    epochs = 10
+    steps = _get_steps(filenames, batch_size)
+
+    print(steps)
+
+    train_dataset = get_dataset_from_csv_files(
+        filenames, 
         left_columns, 
-        right_columns, 
+        right_columns,
         csv_header, 
-        batch_size=256
+        batch_size=batch_size
     )
 
     left_model, right_model = build_model()
     
-    print(left_model.get_weights()[-2])
-    print(right_model.get_weights()[-2])
+    print(left_model.get_weights()[0])
+    print(right_model.get_weights()[0])
 
-    left_model, right_model = custom_train_model(
+    left_model, right_model = train_model(
         left_model, 
         right_model, 
-        train_dataset=train_dataset, 
-        train_steps=math.ceil(712877/256),
-        valid_dataset=valid_dataset, 
-        valid_steps=math.ceil(350730/256),
+        train_dataset, 
+        steps,
+        epochs=epochs,
         ids_column='cand_id',
-        ids_hash_bucket_size=100000,
-        num_epochs=10
+        ids_hash_bucket_size=100000
     )
 
-    print(left_model.get_weights()[-2])
-    print(right_model.get_weights()[-2])
+    print(left_model.get_weights()[0])
+    print(right_model.get_weights()[0])
 
 
