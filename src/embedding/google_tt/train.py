1921d76a0d6ef6d4a26ef6f2a1e9ffa565ad1eaa
王垚 <wangyao.sr@gmail.com>
2020-09-07 19:21:49 +0800

    Debug

--- src/embedding/google_tt/train.py
+++ src/embedding/google_tt/train.py
@@ -5,7 +5,7 @@
 @Author: Wang Yao
 @Date: 2020-08-26 20:47:47
 @LastEditors: Wang Yao
-@LastEditTime: 2020-09-07 19:18:50
+@LastEditTime: 2020-09-07 19:21:43
 """
 import functools
 import numpy as np
@@ -199,7 +199,7 @@ def train_model(dataset,
         @tf.function
         def distributed_train_step(inputs, sampling_p):
             per_replica_losses = strategy.run(train_step, args=(inputs, sampling_p,))
-            return strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_losses, axis=0)
+            return strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_losses, axis=None)
             
         array_a = np.zeros(shape=(ids_hash_bucket_size,), dtype=np.float32)
         array_b = np.ones(shape=(ids_hash_bucket_size,), dtype=np.float32) * beta
