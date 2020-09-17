77954d2df7d87eb48601bd90f51ab5f0d185baa6
王垚 <wangyao.sr@gmail.com>
2020-09-17 16:46:40 +0800

    Fix

--- examples/google_tt/indexing.py
+++ examples/google_tt/indexing.py
@@ -5,7 +5,7 @@
 @Author: Wang Yao
 @Date: 2020-04-30 15:18:32
 @LastEditors: Wang Yao
-@LastEditTime: 2020-09-17 16:43:23
+@LastEditTime: 2020-09-17 16:46:35
 """
 import os
 import sys
@@ -135,9 +135,9 @@ for _, candidates, _ in dataset:
             if len(v.shape) > 1:
                 candidates_data[k] = [[d.decode('utf-8') for d in x] for x in v.numpy()]
             else:
-                candidates_data[k] = [x.decode('utf-8') for x in v.numpy()]
+                candidates_data[k] = [[x.decode('utf-8')] for x in v.numpy()]
         else:
-            candidates_data[k] = v.numpy().astype('float').tolist()
+            candidates_data[k] = v.numpy().astype('float').reshape((-1, 1)).toslit()
 
     data = json.dumps({"signature": "serving_default", "inputs": candidates_data})
 
