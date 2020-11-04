8d39cf387bc1115eec977bbc2185d5ac98d75cc0
王垚 <wangyao.sr@gmail.com>
2020-11-04 18:51:59 +0800

    Change and merge files

--- deep_recommend/recommend/match/google_tt/custom_train.py
+++ deep_recommend/recommend/match/google_tt/custom_train.py
@@ -5,15 +5,17 @@
 @Author: Wang Yao
 @Date: 2020-08-26 20:47:47
 @LastEditors: Wang Yao
-@LastEditTime: 2020-09-27 17:17:42
+@LastEditTime: 2020-11-04 18:50:55
 """
 import os
 import time
+import math
+import pathlib
 import functools
 import numpy as np
 import tensorflow as tf
 
-from src.embedding.google_tt.modeling import build_model
+from modeling import build_model
 
 
 def parse_csv_line(left_columns,
@@ -174,7 +176,7 @@ def topk_positive(output, reward, k=10):
     return tf.cond(tf.math.count_nonzero(reward) > 0, lambda: _ture(reward, indices), lambda: _false())
     
 
-def train_model(strategy,
+def custom_train(strategy,
                 dataset, 
                 steps,
                 epochs,
@@ -324,3 +326,141 @@ def train_model(strategy,
                 
     return left_model, right_model
 
+
+def distribute_train_model(dataset_config, train_config):
+    """分布式训练模型"""
+
+    data_dir = dataset_config.get('data_dir')
+    data_dir = pathlib.Path(data_dir)
+    filenames = sorted([str(fn) for fn in data_dir.glob("*.csv")])[-7:]
+    print("Train filenames: ")
+    for fn in filenames:
+        print(fn)
+    global_batch_size = dataset_config.get('batch_size') * train_config.get('workers_num')
+
+    version = train_config.get('version')
+    tensorboard_dir = os.path.join(train_config.get('tensorboard_dir'), version)
+    checkpoints_dir = os.path.join(train_config.get('checkpoints_dir'), version)
+    query_saved_path = os.path.join(train_config.get('query_saved_path'), version)
+    candidate_saved_path = os.path.join(train_config.get('candidate_saved_path'), version)
+    
+    strategy = tf.distribute.MirroredStrategy()
+
+    train_dataset = get_dataset_from_csv_files(
+        filenames, 
+        dataset_config.get('query_columns'), 
+        dataset_config.get('candidate_columns'),
+        dataset_config.get('csv_header'), 
+        batch_size=global_batch_size
+    )
+
+    def _get_steps(fns, batch_size, skip_header=True):
+        """获取数据集迭代步数"""
+        _total_num = 0
+        for fn in fns:
+            cmd = "wc -l < {}".format(fn)
+            cmd_res = os.popen(cmd)
+            _num_lines = int(cmd_res.read().strip())
+            if skip_header is True:
+                _num_lines -= 1
+            _total_num += _num_lines
+        _steps = math.ceil(_total_num / batch_size)
+        return _steps
+
+    train_steps = _get_steps(filenames, global_batch_size)
+    
+    query_model, candidate_model = custom_train(
+        strategy,
+        train_dataset, 
+        train_steps,
+        epochs=train_config.get('epochs'),
+        ids_column=train_config.get('ids_column'),
+        ids_hash_bucket_size=train_config.get('ids_hash_bucket_size'),
+        tensorboard_dir=tensorboard_dir,
+        checkpoints_dir=checkpoints_dir
+    )
+    
+    print("Saving query model ... ")
+    query_model.save(query_saved_path)
+    print("Query model saved.")
+    print("Saving candidate model ... ")
+    candidate_model.save(candidate_saved_path)
+    print("Candidate model saved.")
+
+
+if __name__ == "__main__":
+
+    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+    query_columns = [
+        'past_watches',
+        'seed_id',
+        'seed_category',
+        'seed_tags',
+        'seed_gap_time',
+        'seed_duration_time',
+        'seed_play_count',
+        'seed_like_count',
+        'seed_share_count',
+        'seed_collect_count'
+    ]
+    candidate_columns = [
+        'cand_id',
+        'cand_category',
+        'cand_tags',
+        'cand_gap_time',
+        'cand_duration_time',
+        'cand_play_count',
+        'cand_like_count',
+        'cand_share_count',
+        'cand_collect_count'
+    ]
+    csv_header = [
+        'label',
+        'udid',
+        'past_watches',
+        'seed_id',
+        'seed_category',
+        'seed_tags',
+        'seed_gap_time',
+        'seed_duration_time',
+        'seed_play_count',
+        'seed_like_count',
+        'seed_share_count',
+        'seed_collect_count',
+        'cand_id',
+        'cand_category',
+        'cand_tags',
+        'cand_gap_time',
+        'cand_duration_time',
+        'cand_play_count',
+        'cand_like_count',
+        'cand_share_count',
+        'cand_collect_count'
+    ]
+
+    dataset_config = {
+        'data_dir': '/home/xddz/data/two_tower_data',
+        'batch_size': 512,
+        'query_columns': query_columns,
+        'candidate_columns': candidate_columns,
+        'csv_header': csv_header
+    }
+    
+    train_config = {
+        'workers_num': 2,
+        'epochs': 10,
+        'ids_column': 'cand_id',
+        'ids_hash_bucket_size': 200000,
+        'version': '20200928',
+        'tensorboard_dir': '/home/xddz/data/two_tower_data/model/training_tensorboard',
+        'checkpoints_dir': '/home/xddz/data/two_tower_data/model/training_checkpoints',
+        'query_saved_path': '/home/xddz/data/two_tower_data/model/models/google_tt_query',
+        'candidate_saved_path': '/home/xddz/data/two_tower_data/model/models/google_tt_candidate'
+    }
+
+    distribute_train_model(
+        dataset_config,
+        train_config
+    )
+    
