aace1f22b35b67da12a9ff3102bac3004b86fe1d
王垚 <wangyao.sr@gmail.com>
2020-11-27 11:04:40 +0800

    Add xDeepFM module

---
+++ deep_recommend/recommend/ctr/xdeepfm/__init__.py
@@ -0,0 +1,9 @@
+"""
+@Description: eXtreme Deep Factorization Machine (xDeepFM)
+@version: 
+@License: MIT
+@Author: Wang Yao
+@Date: 2020-11-27 11:02:30
+@LastEditors: Wang Yao
+@LastEditTime: 2020-11-27 11:04:17
+"""
