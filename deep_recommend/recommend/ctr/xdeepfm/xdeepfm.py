8fe81f3d17a697ee53a554de5b64758a53986703
王垚 <wangyao.sr@gmail.com>
2020-12-01 17:52:07 +0800

    Add xdeepfm module and yaml config

--- deep_recommend/recommend/ctr/xdeepfm/xdeepfm.py
+++ deep_recommend/recommend/ctr/xdeepfm/xdeepfm.py
@@ -5,33 +5,26 @@
 @Author: Wang Yao
 @Date: 2020-11-28 11:16:54
 @LastEditors: Wang Yao
-@LastEditTime: 2020-11-30 19:52:13
+@LastEditTime: 2020-12-01 17:39:35
 """
-import numpy as np
 import tensorflow as tf
 from tensorflow.keras import initializers
-from tensorflow.keras.layers import Layer
-from dataset.criteo import create_feature_layers
+from tensorflow.keras.layers import Layer, Concatenate
 
 
-class CompressedInteraction(Layer):
+class CinBlock(Layer):
     """ Compressed Interaction Layer """
     def __init__(self, num_feature_map, **kwargs):
+        super(CinBlock, self).__init__(**kwargs)
         self._num_feature_map = num_feature_map
-        super(CompressedInteraction, self).__init__(**kwargs)
-
+        
     def build(self, input_shape):
         self.W = self.add_weight(
-            shape=(
-                1,
-                input_shape[0][1] * input_shape[-1][1],
-                self._num_feature_map, 
-            ), 
+            shape=(1, input_shape[0][1] * input_shape[-1][1], self._num_feature_map,), 
             initializer=initializers.glorot_uniform,
             regularizer=None,
             trainable=True,
-            name="filter"
-        )
+            name="filter")
         self.built = True
         
     def call(self, inputs):
@@ -46,43 +39,30 @@ class CompressedInteraction(Layer):
         outputs = tf.nn.sigmoid(filter_out)
         outputs = tf.transpose(outputs, perm=[0, 2, 1])
         return outputs
-        
 
-class SumPooling(Layer):
 
-    def call(self, inputs):
+class CIN(object):
+    """ build Compressed Intercation Network """
+
+    def __init__(self, feature_maps: list, feature_embedding_dim: int, **kwargs):
+        super(CIN, self).__init__(**kwargs)
+        self._feature_maps = feature_maps
+        self._feature_embedding_dim = feature_embedding_dim
+
+    def __call__(self, concat_embeddings):
+        """ build model """
+        features_num = concat_embeddings.shape[-1]//self._feature_embedding_dim
+        x0 = tf.reshape(concat_embeddings, (-1, features_num, self._feature_embedding_dim))
+        x = x0
+        cin_sumpoolings = []
+        for h in self._feature_maps:
+            x = CinBlock(int(h))([x, x0])
+            x_sumpooling = self.sumpooling(x)
+            cin_sumpoolings.append(x_sumpooling)
+        return Concatenate(axis=-1)(cin_sumpoolings)
+
+    def sumpooling(self, inputs):
+        """ Sum pooling through feature maps """
         return tf.reduce_sum(inputs, axis=-1)
 
-
-def build_cin(x0, feature_maps=[3, 3, 3]):
-    """ 构建CIN """
-    x = x0
-    cin_sumpoolings = []
-    for h in feature_maps:
-        x = CompressedInteraction(h)([x, x0])
-        x_sumpooling = SumPooling()(x)
-        cin_sumpoolings.append(x_sumpooling)
-    return tf.keras.layers.Concatenate(axis=-1)(cin_sumpoolings)
-
-
-def build_xdeepfm():
-    """ 构建xdeepfm模型 """
-    (sparse_inputs, sparse_cols), (dense_inputs, dense_cols) = create_feature_layers()
-    
-    features_layer = tf.keras.layers.DenseFeatures(dense_cols + sparse_cols)
-
-    concat_embeddings = features_layer({
-        input_layer.name.split(":")[0]: input_layer
-        for input_layer in dense_inputs + sparse_inputs
-    })
-
-    stack_embeddings = tf.reshape(concat_embeddings, (-1, concat_embeddings.shape[-1]//10, 10))
-
-    cin_output = build_cin(stack_embeddings, feature_maps=[3, 4, 5])
-
-    outputs = tf.keras.layers.Dense(1, activation="sigmoid")(cin_output)
-
-    model = tf.keras.Model(sparse_inputs+dense_inputs, outputs)
-    return model
-    
     
