48c307931e9d11306687c0c4fe53ee19b2d9120c
王垚 <wangyao.sr@gmail.com>
2020-12-03 11:55:56 +0800

    Build DeepFM model in deepfm module

--- deep_recommend/recommend/ctr/deepfm/deepfm.py
+++ deep_recommend/recommend/ctr/deepfm/deepfm.py
@@ -5,10 +5,11 @@
 @Author: Wang Yao
 @Date: 2020-12-01 18:57:30
 @LastEditors: Wang Yao
-@LastEditTime: 2020-12-01 21:45:54
+@LastEditTime: 2020-12-03 11:40:52
 """
 import tensorflow as tf
 from tensorflow.keras.layers import Layer
+from deep_recommend.recommend.ctr.embedding_mlp import EmbeddingMLP
 
 
 class FM(Layer):
@@ -48,4 +49,27 @@ class FmPart(object):
     def __call__(self, concat_embeddings):
         """ build model """
         return FM(self._fm_factors, name=self.__layer__)(concat_embeddings)
-        
+
+
+class DeepFM(object):
+    """ Deep & Factorization Machine """
+    
+    def __init__(self, dataset_config: dict, model_config: dict, **kwargs):
+        super(DeepFM, self).__init__(**kwargs)
+        self._dataset_config = dataset_config
+        self._model_config = model_config
+
+    def __call__(self):
+        embedding_mlp = EmbeddingMLP(
+            self._dataset_config.get("features").get("dense_features"),
+            self._dataset_config.get("features").get("sparse_features"),
+            self._model_config.get("ff").get("hidden_sizes").split(","),
+            self._model_config.get("ff").get("hidden_activation"),
+            self._model_config.get("ff").get("hidden_dropout_rates").split(","),
+            self._model_config.get("logits").get("size"),
+            self._model_config.get("logits").get("activation"),
+            self._model_config.get("model").get("name"),
+            self._model_config.get("model").get("loss"),
+            self._model_config.get("model").get("optimizer")
+        )
+        return embedding_mlp(FmPart(self._model_config.get("fm").get("factors")))
