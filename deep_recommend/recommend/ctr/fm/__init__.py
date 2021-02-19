4241326554c91156fe61dc93d868b2f5484e0286
王垚 <wangyao.sr@gmail.com>
2021-02-19 15:27:29 +0800

    Rename root dir name

--- deep_recommend/recommend/ctr/fm/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-@Description: 
-@version: 
-@License: MIT
-@Author: Wang Yao
-@Date: 2020-12-03 18:00:49
-@LastEditors: Wang Yao
-@LastEditTime: 2020-12-03 18:00:50
-"""
--- deep_recommend/recommend/ctr/xdeepfm/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-@Description:
-@version: 
-@License: MIT
-@Author: Wang Yao
-@Date: 2020-11-27 11:02:30
-@LastEditors: Wang Yao
-@LastEditTime: 2020-11-28 11:25:08
-"""
