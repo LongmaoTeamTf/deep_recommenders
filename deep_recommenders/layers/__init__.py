1d56ae6792fe35231dd1e9a7f689df41d5e16779
王垚 <wangyao.sr@gmail.com>
2021-02-26 14:57:20 +0800

    Add import all layers

---
+++ deep_recommenders/layers/__init__.py
@@ -0,0 +1,14 @@
+"""
+@Description: 
+@version: 
+@License: MIT
+@Author: Wang Yao
+@Date: 2021-02-23 10:48:48
+@LastEditors: Wang Yao
+@LastEditTime: 2021-02-23 10:50:09
+"""
+from deep_recommenders.layers import fm
+from deep_recommenders.layers import dcn_cross
+from deep_recommenders.layers import xdeepfm_cin
+from deep_recommenders.layers import factorized_top_k
+from deep_recommenders.layers import loss
