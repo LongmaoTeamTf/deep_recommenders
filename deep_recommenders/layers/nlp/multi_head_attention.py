ef1fdded49053f8f4a60df8b63d1e0e1be96471d
王垚 <wangyao.sr@gmail.com>
2021-02-26 18:57:33 +0800

    Create nlp layers module, add multi-head-attention

---
+++ deep_recommenders/layers/nlp/multi_head_attention.py
@@ -0,0 +1,158 @@
+'''
+@Description: 
+@version: 
+@License: MIT
+@Author: Wang Yao
+@Date: 2020-03-22 17:48:05
+@LastEditors: Wang Yao
+@LastEditTime: 2020-03-26 18:35:10
+'''
+import os
+import tensorflow as tf
+import tensorflow.keras.backend as K
+
+
+class Embedding(tf.keras.layers.Layer):
+
+    def __init__(self, vocab_size, model_dim, **kwargs):
+        self._vocab_size = vocab_size
+        self._model_dim = model_dim
+        super(Embedding, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.embeddings = self.add_weight(
+            shape=(self._vocab_size, self._model_dim),
+            initializer='glorot_uniform',
+            name="embeddings")
+        super(Embedding, self).build(input_shape)
+
+    def call(self, inputs):
+        if K.dtype(inputs) != 'int32':
+            inputs = K.cast(inputs, 'int32')
+        embeddings = K.gather(self.embeddings, inputs)
+        embeddings *= self._model_dim ** 0.5 # Scale
+        return embeddings
+
+    def compute_output_shape(self, input_shape):
+
+        return input_shape + (self._model_dim,)
+
+
+class ScaledDotProductAttention(tf.keras.layers.Layer):
+
+    def __init__(self, masking=True, future=False, dropout_rate=0., **kwargs):
+        self._masking = masking
+        self._future = future
+        self._dropout_rate = dropout_rate
+        self._masking_num = -2**32+1
+        super(ScaledDotProductAttention, self).__init__(**kwargs)
+
+    def mask(self, inputs, masks):
+        masks = K.cast(masks, 'float32')
+        masks = K.tile(masks, [K.shape(inputs)[0] // K.shape(masks)[0], 1])
+        masks = K.expand_dims(masks, 1)
+        outputs = inputs + masks * self._masking_num
+        return outputs
+    
+    def future_mask(self, inputs):
+        diag_vals = tf.ones_like(inputs[0, :, :])
+        tril = tf.linalg.LinearOperatorLowerTriangular(diag_vals).to_dense()  
+        future_masks = tf.tile(tf.expand_dims(tril, 0), [tf.shape(inputs)[0], 1, 1])
+        paddings = tf.ones_like(future_masks) * self._masking_num
+        outputs = tf.where(tf.equal(future_masks, 0), paddings, inputs)
+        return outputs
+
+    def call(self, inputs):
+        if self._masking:
+            assert len(inputs) == 4, "inputs should be set [queries, keys, values, masks]."
+            queries, keys, values, masks = inputs
+        else:
+            assert len(inputs) == 3, "inputs should be set [queries, keys, values]."
+            queries, keys, values = inputs
+
+        if K.dtype(queries) != 'float32':  queries = K.cast(queries, 'float32')
+        if K.dtype(keys) != 'float32':  keys = K.cast(keys, 'float32')
+        if K.dtype(values) != 'float32':  values = K.cast(values, 'float32')
+
+        matmul = K.batch_dot(queries, tf.transpose(keys, [0, 2, 1])) # MatMul
+        scaled_matmul = matmul / int(queries.shape[-1]) ** 0.5  # Scale
+        if self._masking:
+            scaled_matmul = self.mask(scaled_matmul, masks) # Mask(opt.)
+
+        if self._future:
+            scaled_matmul = self.future_mask(scaled_matmul)
+
+        softmax_out = K.softmax(scaled_matmul) # SoftMax
+        # Dropout
+        out = K.dropout(softmax_out, self._dropout_rate)
+        
+        outputs = K.batch_dot(out, values)
+
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+
+    def __init__(self, n_heads, head_dim, dropout_rate=.1, masking=True, future=False, trainable=True, **kwargs):
+        self._n_heads = n_heads
+        self._head_dim = head_dim
+        self._dropout_rate = dropout_rate
+        self._masking = masking
+        self._future = future
+        self._trainable = trainable
+        super(MultiHeadAttention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self._weights_queries = self.add_weight(
+            shape=(input_shape[0][-1], self._n_heads * self._head_dim),
+            initializer='glorot_uniform',
+            trainable=self._trainable,
+            name='weights_queries')
+        self._weights_keys = self.add_weight(
+            shape=(input_shape[1][-1], self._n_heads * self._head_dim),
+            initializer='glorot_uniform',
+            trainable=self._trainable,
+            name='weights_keys')
+        self._weights_values = self.add_weight(
+            shape=(input_shape[2][-1], self._n_heads * self._head_dim),
+            initializer='glorot_uniform',
+            trainable=self._trainable,
+            name='weights_values')
+        super(MultiHeadAttention, self).build(input_shape)
+
+
+    def call(self, inputs):
+        if self._masking:
+            assert len(inputs) == 4, "inputs should be set [queries, keys, values, masks]."
+            queries, keys, values, masks = inputs
+        else:
+            assert len(inputs) == 3, "inputs should be set [queries, keys, values]."
+            queries, keys, values = inputs
+        
+        queries_linear = K.dot(queries, self._weights_queries) 
+        keys_linear = K.dot(keys, self._weights_keys)
+        values_linear = K.dot(values, self._weights_values)
+
+        queries_multi_heads = tf.concat(tf.split(queries_linear, self._n_heads, axis=2), axis=0)
+        keys_multi_heads = tf.concat(tf.split(keys_linear, self._n_heads, axis=2), axis=0)
+        values_multi_heads = tf.concat(tf.split(values_linear, self._n_heads, axis=2), axis=0)
+        
+        if self._masking:
+            att_inputs = [queries_multi_heads, keys_multi_heads, values_multi_heads, masks]
+        else:
+            att_inputs = [queries_multi_heads, keys_multi_heads, values_multi_heads]
+            
+        attention = ScaledDotProductAttention(
+            masking=self._masking, future=self._future, dropout_rate=self._dropout_rate)
+        att_out = attention(att_inputs)
+
+        outputs = tf.concat(tf.split(att_out, self._n_heads, axis=0), axis=2)
+        
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+        
