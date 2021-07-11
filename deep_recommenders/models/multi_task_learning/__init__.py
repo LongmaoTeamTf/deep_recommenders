153205ee8aaf8fd0482180c014bdde7d538512b6
王垚 <wangyao.sr@gmail.com>
2021-07-11 18:10:27 +0800

    add synthetic_data_input_fn in mixture_of_experts

--- deep_recommenders/models/multi_task_learning/__init__.py
+++ deep_recommenders/models/multi_task_learning/__init__.py
@@ -3,3 +3,4 @@
 
 from deep_recommenders.models.multi_task_learning.shared_bottom import shared_bottom
 from deep_recommenders.models.multi_task_learning.shared_bottom import shared_bottom_estimator
+from deep_recommenders.models.multi_task_learning.mixture_of_experts import synthetic_data_input_fn
