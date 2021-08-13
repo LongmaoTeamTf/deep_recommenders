f3a6f957c19daa77af9f15255dfe366cee16a7b0
GrogusBall <wangyao.sr@gmail.com>
2021-08-13 22:44:10 +0800

    Fix code format issues

--- tests/testing.sh
+++ tests/testing.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 function testing() {
-  for file in $(ls $1); do
+  for file in $(ls "$1"); do
     if [ "${file##*.}"x = "py"x ]; then
-      python $1/$file >/dev/null 2>&1
+      python "$1"/"$file" >/dev/null 2>&1
       if [ $? -eq 0 ]; then
-        echo -e "Test[OK]" $1/$file
+        echo -e "Test[OK]" "$1"/"$file"
       else
-        echo -e "Test[ERROR]" $1/$file
+        echo -e "Test[ERROR]" "$1"/"$file"
         exit 1
       fi
     fi
   done
 }
 
-testing tests/$1
+testing tests/"$1"
