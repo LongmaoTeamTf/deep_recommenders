0294d2c1750f163e418cefd2253036a8ea92441f
GrogusBall <wangyao.sr@gmail.com>
2021-08-13 22:37:48 +0800

    Fix code format issues

--- tests/testing.sh
+++ tests/testing.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-function testing(){
-    for file in `ls $1`
-    do
-      if [ "${file##*.}"x = "py"x ];then
-        python $1/$file >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo -e "Test[OK]" $1/$file
-        else
-            echo -e "Test[ERROR]" $1/$file
-            exit 1
-        fi
+function testing() {
+  for file in $(ls $1); do
+    if [ "${file##*.}"x = "py"x ]; then
+      python $1/$file >/dev/null 2>&1
+      if [ $? -eq 0 ]; then
+        echo -e "Test[OK]" $1/$file
+      else
+        echo -e "Test[ERROR]" $1/$file
+        exit 1
       fi
-    done
+    fi
+  done
 }
 
 testing tests/$1
-
-
-
-
-
