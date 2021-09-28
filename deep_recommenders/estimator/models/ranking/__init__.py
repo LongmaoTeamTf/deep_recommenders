979b7a14fda77c4d531964cb895edbf9dcce4f67
GrogusBall <wangyao.sr@gmail.com>
2021-09-28 17:37:09 +0800

    Add Wide & Deep ranking model.

--- deep_recommenders/estimator/models/ranking/__init__.py
+++ deep_recommenders/estimator/models/ranking/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from deep_recommenders.estimator.models.ranking.wide_and_deep import WDL
 from deep_recommenders.estimator.models.ranking.deepfm import DeepFM
