5f9b591f075d8ffd51c71d4c1c50da6836fad12f
GrogusBall <wangyao.sr@gmail.com>
2021-10-19 16:01:48 +0800

    Add ESMM model for multi task learning.

---
+++ deep_recommenders/estimator/models/multi_task_learning/esmm.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+
+from deep_recommenders.estimator.models.feature_interaction import dnn
+
+
+class ESMM(object):
+
+    def __init__(self,
+                 feature_columns,
+                 hidden_units,
+                 activation=tf.nn.relu,
+                 batch_normalization=False,
+                 dropout=None,
+                 **kwargs):
+        self._feature_columns = feature_columns
+        self._hidden_units = hidden_units
+        self._activation = activation
+        self._batch_norm = batch_normalization
+        self._dropout = dropout
+        self._configs = kwargs
+
+    def __call__(self, *args, **kwargs):
+        return self.call(*args, **kwargs)
+
+    def call(self, features):
+
+        dnn_inputs = tf.feature_column.input_layer(features,
+                                                   self._numerical_columns)
+
+        with tf.variable_scope("pCVR"):
+            cvr = dnn(dnn_inputs,
+                      self._hidden_units,
+                      activation=self._activation,
+                      batch_normalization=self._batch_norm,
+                      dropout=self._dropout,
+                      **self._configs)
+            p_cvr = tf.nn.sigmoid(cvr)
+
+        with tf.variable_scope("pCTR"):
+            ctr = dnn(dnn_inputs,
+                      self._hidden_units,
+                      activation=self._activation,
+                      batch_normalization=self._batch_norm,
+                      dropout=self._dropout,
+                      **self._configs)
+            p_ctr = tf.nn.sigmoid(ctr)
+
+        p_ctcvr = tf.math.multiply(p_ctr, p_cvr)
+
+        return p_cvr, p_ctr, p_ctcvr
