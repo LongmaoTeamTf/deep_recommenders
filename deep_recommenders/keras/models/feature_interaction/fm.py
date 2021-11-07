a95eac36b0aa683b794c418f058794545717492d
GrogusBall <wangyao.sr@gmail.com>
2021-11-07 21:15:59 +0800

    Debug Keras layer register.

--- deep_recommenders/keras/models/feature_interaction/fm.py
+++ deep_recommenders/keras/models/feature_interaction/fm.py
@@ -4,7 +4,7 @@
 import tensorflow as tf
 
 
-@tf.keras.utils.register_keras_serializable()
+# @tf.keras.utils.register_keras_serializable()
 class FM(tf.keras.layers.Layer):
     """ Factorization Machine """
 
